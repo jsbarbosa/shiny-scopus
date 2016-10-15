c826994ba84b11c6da43c27359449c891a479e81
jsbarbosa <js.barbosa10@uniandes.edu.co>
2016-10-15 18:41:55 -0500

    included first draft

---
+++ scopus_exercise.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Oct 11 14:15:04 2016
+
+@author: juan
+"""
+
+import requests
+import json
+#from my_scopus import MY_API_KEY
+
+from scopus.scopus_api import ScopusAbstract
+
+
+ab = ScopusAbstract("2-s2.0-84930616647")
+print(ab.bibtex)
+print(ab.ris)
